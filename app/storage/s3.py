817475dc62fde976b6687bd79302f7a28a1c6f05
arv <61883142+onedownz01@users.noreply.github.com>
2026-01-11 17:10:42 +0530

    Update s3.py

--- app/storage/s3.py
+++ app/storage/s3.py
@@ -1,16 +1,40 @@
 import boto3
-import os
+import uuid
+from app.config import (
+    AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY,
+    AWS_REGION,
+    S3_BUCKET_NAME
+)
 
 s3 = boto3.client(
     "s3",
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    region_name=os.getenv("AWS_REGION"),
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_REGION
 )
 
-BUCKET = os.getenv("S3_BUCKET_NAME")
+def upload_file_to_s3(file_bytes: bytes, vault_id: str, filename: str, content_type: str):
+    key = f"vaults/{vault_id}/raw/{uuid.uuid4()}_{filename}"
 
-def s3_health_check():
-    s3.head_bucket(Bucket=BUCKET)
-    return True
+    s3.put_object(
+        Bucket=S3_BUCKET_NAME,
+        Key=key,
+        Body=file_bytes,
+        ContentType=content_type
+    )
 
+    return {
+        "bucket": S3_BUCKET_NAME,
+        "key": key
+    }
+
+def generate_presigned_url(key: str, expires_in: int = 3600):
+    return s3.generate_presigned_url(
+        "get_object",
+        Params={
+            "Bucket": S3_BUCKET_NAME,
+            "Key": key
+        },
+        ExpiresIn=expires_in
+    )
