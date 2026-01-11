452d31f0dff1b270d8d6d474efd0a3441b080f2e
arv <61883142+onedownz01@users.noreply.github.com>
2026-01-11 16:48:33 +0530

    Update s3.py

--- app/storage/s3.py
+++ app/storage/s3.py
@@ -1,1 +1,16 @@
+import boto3
+import os
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_REGION"),
+)
+
+BUCKET = os.getenv("S3_BUCKET_NAME")
+
+def s3_health_check():
+    s3.head_bucket(Bucket=BUCKET)
+    return True
 
